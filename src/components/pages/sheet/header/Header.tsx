95974bbda2ff92a33bc75dd8543560f2e08fce08
iuliailies <72351491+iuliailies@users.noreply.github.com>
2023-10-03 12:03:39 +0300

    feat(sheets): create dropdown wrapper

--- src/components/pages/sheet/header/Header.tsx
+++ src/components/pages/sheet/header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { icon, regular } from '@fortawesome/fontawesome-svg-core/import.macro'
 import './Header.sass'
@@ -9,42 +9,81 @@ interface HeaderProps {
     onDateChange: (newDate: Date) => void;
   }
 
-  const Header: React.FC<HeaderProps> = ({
-    currentMonth,
-    currentYear,
-    onDateChange
-  }) => {
-    const handlePrevMonth = () => {
-        const newDate = new Date(currentYear, currentMonth - 1, 1);
-        onDateChange(newDate);
-      };
+const Header: React.FC<HeaderProps> = ({
+  currentMonth,
+  currentYear,
+  onDateChange
+}) => {
+  const [isDropdownOpen, setDropdownOpen] = useState<boolean>(false);
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+  const iconRef = useRef<SVGSVGElement | null>(null); // refferences the icon that triggers the dropdown
 
-      const handleNextMonth = () => {
-        const newDate = new Date(currentYear, currentMonth + 1, 1);
-        onDateChange(newDate);
-      };
+  const handlePrevMonth = () => {
+      const newDate = new Date(currentYear, currentMonth - 1, 1);
+      onDateChange(newDate);
+    };
 
-    return (
-      <header>
-        <FontAwesomeIcon
-          className="icon"
-          onClick={handlePrevMonth}
-          icon={icon({ name: 'chevron-left' })}
-        />
-        <FontAwesomeIcon
-          className="icon"
-          onClick={handleNextMonth}
-          icon={icon({ name: 'chevron-right' })}
-        />
-        <div className="title">
-          {new Intl.DateTimeFormat('en-US', {
-            year: 'numeric',
-            month: 'long',
-          }).format(new Date(currentYear, currentMonth, 1))}
-        </div>
-        <FontAwesomeIcon className="icon" icon={regular('calendar-plus')} />
-      </header>
-    );
+  const handleNextMonth = () => {
+  const newDate = new Date(currentYear, currentMonth + 1, 1);
+  onDateChange(newDate);
   };
 
-  export default Header;
+  const handleToggleDropdown = (event: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+      event.stopPropagation();
+      setDropdownOpen(prevIsDropdownOpen => !prevIsDropdownOpen);
+  }
+
+  useEffect(() => {
+      // handles clicks outside of the dropdown
+      const handleClickOutside = (event: MouseEvent) => {
+          if (
+              isDropdownOpen &&
+              dropdownRef.current &&
+              !dropdownRef.current.contains(event.target as Node) &&
+              iconRef.current &&
+              !iconRef.current.contains(event.target as Node)
+          ) {
+            setDropdownOpen(false);
+          }
+        };
+
+      document.addEventListener('click', handleClickOutside);
+
+      // clean up the event listener when the component unmounts
+      return () => {
+        document.removeEventListener('click', handleClickOutside);
+      };
+    }, [isDropdownOpen]); // empty dependency array ensures this effect runs only once
+
+
+  return (
+    <header>
+      <FontAwesomeIcon
+        className="icon"
+        onClick={handlePrevMonth}
+        icon={icon({ name: 'chevron-left' })}
+      />
+      <FontAwesomeIcon
+        className="icon"
+        onClick={handleNextMonth}
+        icon={icon({ name: 'chevron-right' })}
+      />
+      <div className="title">
+        {new Intl.DateTimeFormat('en-US', {
+          year: 'numeric',
+          month: 'long',
+        }).format(new Date(currentYear, currentMonth, 1))}
+      </div>
+      <div className="dropdown-wrapper">
+          <FontAwesomeIcon className="icon primary" onClick={handleToggleDropdown} ref={iconRef} icon={regular('calendar-plus')} />
+          {isDropdownOpen && (
+              <div className="dropdown" ref={dropdownRef}>
+
+              </div>
+          )}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
